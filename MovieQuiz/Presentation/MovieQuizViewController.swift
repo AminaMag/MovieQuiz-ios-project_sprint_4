580015ba477cd657b11c44d59ee7ab4057753f25
AminaMag <magomedova_amina2005@mail.ru>
2023-10-15 16:42:48 +0300

    добавлены слабые ссылки во все замыкания в проекте

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -119,7 +119,9 @@ final class MovieQuizViewController: UIViewController {
             title: result.title,
             message: result.text,
             preferredStyle: .alert)
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             
@@ -128,6 +130,10 @@ final class MovieQuizViewController: UIViewController {
             self.show(quiz: viewModel)
         }
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
+            self.showNextQuestionOrResult()
+        }
         alert.addAction(action)
         
         self.present(alert, animated: true, completion: nil)
